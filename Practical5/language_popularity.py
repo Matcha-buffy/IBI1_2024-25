9ced2e9491ad5e9161f65c43f3193bbe3366a59d
Matcha-buffy <tianze.24@intl.zju.edu.cn>
2025-03-19 15:22:13 +0800

    Update language_popularity.py

--- Practical5/language_popularity.py
+++ Practical5/language_popularity.py
@@ -4,8 +4,8 @@ language_popularity = {
     "Python": 51,
     "SQL": 51,
     "TypeScript": 38.5
-} # PSEUDOCODE: REQUESTED_LANGUAGE_VARIABLE
-import matplotlib.pyplot as plt
+}#give the varieable
+import matplotlib.pyplot as plt#make the chart
 
 languages = list(language_popularity.keys())
 percentages = list(language_popularity.values())
@@ -13,5 +13,10 @@ percentages = list(language_popularity.values())
 plt.bar(languages, percentages)
 plt.xlabel("Programming Languages") 
 plt.ylabel("Percentage of Developers")
-plt.title("Programming Language Popularity")
-plt.show()
+plt.title("Programming Language Popularity")#name the x,y,give title
+plt.show() 
+requested_language = "Python" # if request language, give the number
+if requested_language in language_popularity:
+    print(f"The percentage of developers who use {requested_language} is {language_popularity[requested_language]}%")
+else:
+    print("Language not found in the list.")

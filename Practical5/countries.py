329dc245a3729c24172816d156c5a1ce841a3805
Matcha-buffy <tianze.24@intl.zju.edu.cn>
2025-03-19 15:22:24 +0800

    Update countries.py

--- Practical5/countries.py
+++ Practical5/countries.py
@@ -1,11 +1,11 @@
 uk_countries = [57.11, 3.13, 1.91, 5.45]
-provinces = [65.77, 41.88, 45.28, 61.27, 85.15]
+provinces = [65.77, 41.88, 45.28, 61.27, 85.15]#define the list
 uk_countries_sorted = sorted(uk_countries)
 provinces_sorted = sorted(provinces)
 
-print("Sorted UK countries population:", uk_countries_sorted)
+print("Sorted UK countries population:", uk_countries_sorted)#title
 print("Sorted Zhejiang-neighboring provinces population:", provinces_sorted)
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt#make the pie charts
 plt.figure(figsize=(10, 5))
 
 plt.subplot(1, 2, 1)
